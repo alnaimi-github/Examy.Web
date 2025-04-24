1b730053c3a1b2b13bff79ba134ac8eaa74b3166
alnaimi-github <alnymybdalwas1@gmail.com>
2025-04-24 19:00:33 +0300

    Refactor QuizEndpoints and add Swagger support
    Refactored `QuizEndpoints.cs` by renaming `quizGroup` to `quizgroup` for consistency and removing a duplicate `MapGet` method. Enhanced `Program.cs` with Swagger/OpenAPI configuration, added comments for clarity, and prepared for future service additions. Introduced a basic structure in `HomeLayout.razor` with a new header and empty `@code` block.

--- Examy.Api/Endpoints/QuizEndpoints.cs
+++ Examy.Api/Endpoints/QuizEndpoints.cs
@@ -7,9 +7,9 @@ public static class QuizEndpoints
 {
     public static IEndpointRouteBuilder MapQuizEndpoints(this IEndpointRouteBuilder app)
     {
-        var quizGroup = app.MapGroup("/api/quizes").RequireAuthorization();
+        var quizgroup = app.MapGroup("/api/quizes").RequireAuthorization();
 
-        quizGroup.MapPost("", async ( QuizSaveDto dto, QuizService service) =>
+        quizgroup.MapPost("", async ( QuizSaveDto dto, QuizService service) =>
         {
             if(dto.Questions.Count == 0)
             {
@@ -23,30 +23,25 @@ public static class QuizEndpoints
             return Results.Ok(await service.SaveQuizAsync(dto));
         });
 
-        quizGroup.MapGet("", async (QuizService service) 
+        quizgroup.MapGet("", async (QuizService service) 
             => Results.Ok(await service.GetQuizesAsync()));
 
-        quizGroup.MapGet("{quizId:guid}/questions", async (Guid quizId, QuizService service) =>
+        quizgroup.MapGet("{quizId:guid}/questions", async (Guid quizId, QuizService service) =>
         {
             return Results.Ok(await service.GetQuizQuestions(quizId));
         });
 
-        quizGroup.MapGet("{quizId:guid}/questions", async (Guid quizId, QuizService service) =>
+        quizgroup.MapGet("{quizId:guid}", async (Guid quizId, QuizService service) =>
         {
-            return Results.Ok(await service.GetQuizQuestions(quizId));
+            return Results.Ok(await service.GetQuizToEditAsync(quizId));    
         });
 
-        quizGroup.MapGet("{quizId:guid}", async (Guid quizId, QuizService service) =>
-        {
-            return Results.Ok(await service.GetQuizToEditAsync(quizId));
-        });
-
-        quizGroup.MapDelete("{quizId:guid}", async (Guid quizId, QuizService service) =>
+        quizgroup.MapDelete("{quizId:guid}", async (Guid quizId, QuizService service) =>
         {
             return Results.Ok(await service.RemoveQuizAsync(quizId));
         });
 
-        quizGroup.MapDelete("{quizId:guid}/questions/{questionId}", async (Guid quizId, int questionId, QuizService service) =>
+        quizgroup.MapDelete("{quizId:guid}/questions/{questionId}", async (Guid quizId, int questionId, QuizService service) =>
         {
             return Results.Ok(await service.RemoveQuestionAsync(quizId, questionId));
         });
